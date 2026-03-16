950d6a7fdc65aa121866a62715623b84f705067a
i369 Developer <i369.developer@gmail.com>
2026-03-16 22:50:33 +0000

    use beautiful button style (_for element <SidebarMenu>_)

--- src/app/admin/dashboard.tsx
+++ src/app/admin/dashboard.tsx
@@ -211,6 +211,7 @@ export default function Dashboard() {
               </SidebarMenuItem>
             </SidebarMenu>
           </SidebarContent>
+          <SidebarSeparator />
           <SidebarFooter>
             <SidebarMenu>
                 <SidebarMenuItem>
